49dde4af04c25cbbaaf216e9b4e2fd33ec827b43
Laudari Sudip <53330430+Laudarisd@users.noreply.github.com>
2021-04-19 17:38:29 +0900

    Add files via upload

--- Object-Detection/YOLO/Yolo-V4/video_inference.py
+++ Object-Detection/YOLO/Yolo-V4/video_inference.py
@@ -1,139 +1,89 @@
 from ctypes import *
-import glob
-import math
 import random
 import os
 import cv2
-import numpy as np
 import time
 import darknet
-import collections
+import argparse
+from threading import Thread, enumerate
+from queue import Queue
+import datetime
 
-def convertBack(x, y, w, h):
-    xmin = int(round(x - (w / 2)))
-    xmax = int(round(x + (w / 2)))
-    ymin = int(round(y - (h / 2)))
-    ymax = int(round(y + (h / 2)))
-    return xmin, ymin, xmax, ymax
+# weight_file = "yolov4.weights"
+# config_file = "./cfg/yolov4.cfg"
+# data_file = "./cfg/coco.data"
 
+# weight_file = "/data/backup/darknet/weights/COCO2017_2020.12.17/yolov4-custom_coco_last.weights"
+# config_file = "/data/backup/darknet/build/darknet/x64/cfg/yolov4-custom_coco.cfg"
+# data_file = "/data/backup/darknet/build/darknet/x64/data/coco2017_obj.data"
 
-def cvDrawBoxes(detections, img):
-    labels = []
-    for detection in detections:
-        x, y, w, h = detection[2][0],\
-            detection[2][1],\
-            detection[2][2],\
-            detection[2][3]
-        xmin, ymin, xmax, ymax = convertBack(float(x), float(y), float(w), float(h))
-        pt1 = (xmin, ymin)
-        pt2 = (xmax, ymax)
+# weight_file = "/data/backup/darknet/weights/VOC2012_2020.12.23/yolov4-custom_voc_last.weights"
+# config_file = "/data/backup/darknet/build/darknet/x64/cfg/yolov4-custom_voc.cfg"
+# data_file = "/data/backup/darknet/build/darknet/x64/data/voc2012_obj.data"
 
-        if xmin >= 0 and ymin >= 0 and xmax >= 0 and ymax >= 0:
-            cv2.rectangle(img, pt1, pt2, (0, 255, 0), 1)
-            cv2.putText(img,
-                        detection[0].decode() +
-                        " [" + str(round(detection[1] * 100, 2)) + "]",
-                        (pt1[0], pt1[1] - 3), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-                        [255, 255, 0], 1)
-            labels.append(detection[0].decode())
+weight_file = "/data/backup/darknet/weights/cigar_2021_01_05/yolov4-custom_cigar_box_last.weights"
+config_file = "/data/backup/darknet/build/darknet/x64/cfg/yolov4-custom_cigar_box.cfg"
+data_file = "/data/backup/darknet/build/darknet/x64/data/cigar_box_obj.data"
+thresh_hold = .7
 
-        else:
-            pass
-        
-    print(collections.Counter(labels))
+network, class_names, class_colors = darknet.load_network(config_file, data_file, weight_file, batch_size=1)
+
+###############################################################################################
+cap = cv2.VideoCapture(-1)
+MJPG_CODEC = 1196444237.0 # MJPG
+cap_AUTOFOCUS = 0
+cap_FOCUS = 0
+#cap_ZOOM = 400
+
+frame_width = int(1920)
+frame_height = int(1080)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
+# cv2.namedWindow('inference', cv2.WINDOW_FREERATIO)
+# cv2.resizeWindow('inference', frame_width, frame_height)
+
+cap.set(cv2.CAP_PROP_BRIGHTNESS, 0)
+cap.set(cv2.CAP_PROP_FOURCC, MJPG_CODEC)
+cap.set(cv2.CAP_PROP_AUTOFOCUS, cap_AUTOFOCUS)
+cap.set(cv2.CAP_PROP_FOCUS, cap_FOCUS)
+##############################################################################################
+width = darknet.network_width(network)
+height = darknet.network_height(network)
+darknet_image = darknet.make_image(width, height, 3)
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    frame_resized = cv2.resize(frame_rgb, (width, height), interpolation=cv2.INTER_LINEAR)
+    darknet.copy_image_from_bytes(darknet_image, frame_resized.tobytes())
     
-    return img
+    detections = darknet.detect_image(network, class_names, darknet_image, thresh=thresh_hold, hier_thresh=.5, nms=.45)
+    # darknet.print_detections(detections)
 
-
-netMain = None
-metaMain = None
-altNames = None
-
-
-
-def YOLO(frame):
-    global metaMain, netMain, altNames
-    configPath = "./test/yolo-obj.cfg"
-    weightPath = "./test/yolo-obj_final.weights"
-    metaPath= "./test/obj.data"
-
-    if not os.path.exists(configPath):
-        raise ValueError("Invalid config path `" +
-                         os.path.abspath(configPath)+"`")
-    if not os.path.exists(weightPath):
-        raise ValueError("Invalid weight path `" +
-                         os.path.abspath(weightPath)+"`")
-    if not os.path.exists(metaPath):
-        raise ValueError("Invalid data file path `" +
-                         os.path.abspath(metaPath)+"`")
-    if netMain is None:
-        netMain = darknet.load_net_custom(configPath.encode(
-            "ascii"), weightPath.encode("ascii"), 0, 1)  # batch size = 1
-    if metaMain is None:
-        metaMain = darknet.load_meta(metaPath.encode("ascii"))
-    if altNames is None:
-        try:
-            with open(metaPath) as metaFH:
-                metaContents = metaFH.read()
-                import re
-                match = re.search("names *= *(.*)$", metaContents,
-                                  re.IGNORECASE | re.MULTILINE)
-                if match:
-                    result = match.group(1)
-                else:
-                    result = None
-                try:
-                    if os.path.exists(result):
-                        with open(result) as namesFH:
-                            namesList = namesFH.read().strip().split("\n")
-                            altNames = [x.strip() for x in namesList]
-                except TypeError:
-                    pass
-        except Exception:
-            pass
-
-    darknet_image = darknet.make_image(darknet.network_width(netMain),
-                                       darknet.network_height(netMain),
-                                       3)
-    image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    image = cv2.resize(image, (darknet.network_width(netMain), darknet.network_height(netMain)))
-    darknet.copy_image_from_bytes(darknet_image, image.tobytes())
-
-    detections = darknet.detect_image(netMain, metaMain, darknet_image, thresh=0.8)
-    image = cvDrawBoxes(detections, image)
-    # image  = cv2.resize(image, (3840, 2160))
-    # image = cv2.resize(image, (1280, 720))
+    image = darknet.draw_boxes(detections, frame_resized, class_colors)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = cv2.putText(image, "Objects : " + str(len(detections)), (width-220, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+    # image = cv2.resize(image, (1280, 720))
+    # print('NUM OF OBJECT :',  len(detections))
 
-    return image
+    cv2.imshow("inference", image)
 
+    k = cv2.waitKey(1)
+    if k == ord('q'):
+        os.system('clear')
+        break
 
-if __name__ == "__main__":
-    cap = cv2.VideoCapture(-1)
-    ####################################################################################################
-    # video option
-    YUV2_CODEC = 844715353.0 # YUY2
-    MJPG_CODEC = 1196444237.0 # MJPG
-    cap_FPS = 15
-    cap_AUTOFOCUS = 0
-    cap_FOCUS = 0
-    cap.set(cv2.CAP_PROP_FOURCC, MJPG_CODEC)
-    cap.set(3, 4096)
-    cap.set(4, 2160)
-    cap.set(cv2.CAP_PROP_FPS, cap_FPS)
-    cap.set(cv2.CAP_PROP_AUTOFOCUS, cap_AUTOFOCUS)
-    cap.set(cv2.CAP_PROP_FOCUS, cap_FOCUS)
-    ####################################################################################################
+    if k == ord('s'):
+        time = datetime.datetime.now().strftime("%Y.%m.%d_%H:%M:%S")
+        cv2.imwrite('/data/backup/pervinco_2020/darknet/results/' + time + '.jpg', image)
+        print(time,'.jpg is saved')
 
-    while  True:
-        ret, frame = cap.read()
+    # if k == ord('t'):
+    #     os.system('clear')
+    #     print('NUM OF OBJECT :',  len(detections))
 
-        # frame = cv2.resize(frame, (1920, 1080))
-        # cv2.imshow('test', frame)
-        detected_frame = YOLO(frame)
-        cv2.imshow('test', detected_frame)
-
-        k = cv2.waitKey(1) & 0xFF
-
-        if k == ord('q'):
-            break
+cap.release()
+cv2.destroyAllWindows()
